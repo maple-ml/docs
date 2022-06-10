c7e9a0bca0fa81d161248f9dc06704c502b3e35c
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2022-06-10 08:55:00 -0700

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+import re
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+autodoc_member_order = "bysource"
+
+intersphinx_mapping = {
+    "py": ("https://docs.python.org/3", None),
+    "aio": ("https://aiohttp.readthedocs.io/en/stable/", None),
+}
+
+rst_prolog = """
+.. |coro| replace:: This function is a |coroutine_link|_.
+.. |maybecoro| replace:: This function *could be a* |coroutine_link|_.
+.. |coroutine_link| replace:: *coroutine*
+.. _coroutine_link: https://docs.python.org/3/library/asyncio-task.html#coroutine
+"""
+
+source_suffix = ".rst"
+
+master_doc = "index"
+
+project = "maple"
+copyright = "2019-2020, Maple-ML team"
+
+language = "en"
+
+pdf_documents = [
+    (
+        "index",
+        "Maple Documentation",
+        "Maple documentation, built with PDF format.",
+    )
+]
+
+exclude_patterns = ["build"]
+
+pygments_style = "friendly"
+
+html_theme = "sphinx_rtd_theme"
